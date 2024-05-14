6b7d36dd435553e637b47859a24934af67f0f3bb
ADARSH SINGH <9792051262a@gmail.com>
2024-05-14 19:40:01 +0530

    Update socket.js

--- socket/socket.js
+++ socket/socket.js
@@ -6,9 +6,9 @@ const app = express();
 
 const server = http.createServer(app);
 const io = new Server(server, {
-    cors:{
-        origin:['http://localhost:3000'],
-        methods:['GET', 'POST'],
+    cors: {
+        origin: ['https://main--conversex.netlify.app', 'https://conversex.netlify.app'],
+        methods: ['GET', 'POST'],
     },
 });
 
